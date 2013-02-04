b46618e8c50889b5d3f56c01cacc7c3dea5fab27
Ian Marvin <ian@dev.argento.co.z>
2013-02-04 15:16:25 +0200

    adding label to org yo

--- lib/gauthic/shared_contact/organization.rb
+++ lib/gauthic/shared_contact/organization.rb
@@ -2,6 +2,8 @@ module Gauthic
   class SharedContact
     class Organization < AbstractNode
       def_attributes :orgName, :orgDepartment, :orgTitle
+      def label()         root['label'] end
+      def label=(value)   root['label'] = value end
     end
   end
 end
